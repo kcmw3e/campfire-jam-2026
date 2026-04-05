2976f3c54b90d64512129b8af87ae88ad9c8f554
Grace Xin <qingqinggrace@gmail.com>
2026-04-05 15:25:32 -0400

    prevent player from moving past edge of environment

--- src/campsite.rs
+++ src/campsite.rs
@@ -5,10 +5,16 @@ use crate::y_sort::*;
 #[derive(Component)]
 pub struct Campsite;
 
+pub const CAMPSITE_SIZE: Vec2 = Vec2::new(3000., 1650.);
+
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         (
-            Sprite::from_image(asset_server.load("campsite_bg.png")),
+            Sprite {
+                custom_size: Some(CAMPSITE_SIZE),
+                image: asset_server.load("campsite_bg.png"),
+                ..default()
+            },
             Transform::from_xyz(0., 0., 0.),
         ),
         Campsite,
