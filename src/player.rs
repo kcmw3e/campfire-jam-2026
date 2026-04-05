906420e39cb4b6249a7f2f368a4ceb267081e027
Grace Xin <qingqinggrace@gmail.com>
2026-04-05 13:17:26 -0400

    display campfire, player on top of background (z=1>0)

--- src/player.rs
+++ src/player.rs
@@ -9,7 +9,7 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         (
             Sprite::from_image(asset_server.load("person.png")),
-            Transform::from_xyz(0., 0., 0.),
+            Transform::from_xyz(0., 0., 1.),
         ),
         Player,
     ));
