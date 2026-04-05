947fe7f16a3787d786e5079224607bac91036701
Grace Xin <qingqinggrace@gmail.com>
2026-04-05 11:49:21 -0400

    add campfire in middle of screen

---
+++ src/campfire.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::*;
+use bevy_easy_gif::*;
+
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Gif {
+        handle: asset_server.load("campfire/campfire_preview.gif"),
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
+use bevy_easy_gif::*;
 
+mod campfire;
 mod player;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(GifPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Startup, campfire::setup)
         .add_systems(Startup, player::setup)
         .add_systems(Update, player::movement)
         .run();
