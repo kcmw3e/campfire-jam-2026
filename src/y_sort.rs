159f14c2014c141a6a2c78d4dfb9bd4404817cbe
Grace Xin <qingqinggrace@gmail.com>
2026-04-06 23:18:40 -0400

    normalize ysort z-band calculations

--- src/y_sort.rs
+++ src/y_sort.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::background::BACKGROUND_SIZE;
+
 #[derive(Component)]
 pub struct YSort {
     pub z: f32,
@@ -7,6 +9,6 @@ pub struct YSort {
 
 pub fn y_sort(mut q: Query<(&mut Transform, &YSort)>) {
     for (mut tf, y_sort) in q.iter_mut() {
-        tf.translation.z = y_sort.z - (tf.translation.y * 0.001);
+        tf.translation.z = y_sort.z - (tf.translation.y / BACKGROUND_SIZE.y);
     }
 }
